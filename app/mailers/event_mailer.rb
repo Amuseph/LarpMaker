65a6b315c00bcbcab2bc7c473456127aa1d79cee
Amuseph <andrew.warzocha@gmail.com>
2021-09-28 00:04:36 -0400

    Feedback letters to Trello

---
+++ app/mailers/event_mailer.rb
@@ -0,0 +1,22 @@
+class EventMailer < ApplicationMailer
+
+  def send_event_feedback
+    @feedback = params[:eventfeedback]
+    puts('TACO')
+    puts('TACO')
+    puts(@feedback)
+    puts('TACO')
+    puts('TACO')
+    puts('TACO')
+    #Andrew Warzocha | (Atrius Athear) | Event: 2021-08-21
+    if !@feedback.character.nil?
+      @subject = @feedback.user.firstname + ' | (' + @feedback.character.name + ') | ' + @feedback.event.startdate.strftime("%m/%d/%Y")
+    else
+      @subject = @feedback.user.firstname + ' | (' + @feedback.event.startdate.strftime("%m/%d/%Y")
+    end
+
+
+
+    mail(from: 'no-reply@mythlarp.com', to: 'gary80740106+kyo5skwxedfyd3ua6swu@boards.trello.com', subject: @subject)
+  end
+end
