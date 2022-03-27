fb1c28308d560b3228d26186289decf68e8f359e
Amuseph <andrew.warzocha@gmail.com>
2022-03-27 14:27:52 -0400

    Updates to Terms of Service

--- app/models/character.rb
+++ app/models/character.rb
@@ -22,6 +22,7 @@ class Character < ApplicationRecord
   validates :name, presence: true
 
   after_update :check_class
+  after_create :check_class
 
   has_one_attached :photo
   validates :photo, content_type: ['image/png', 'image/jpg', 'image/jpeg'], size: { less_than: 15.megabytes , message: 'is not given between size' }
