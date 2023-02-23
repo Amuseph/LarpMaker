6a6fd439942a11bd0d7f1a42bf679c279eb7b791
Amuseph <andrew.warzocha@gmail.com>
2023-02-23 17:45:31 -0500

    Code Cleanup

--- app/helpers/pages_helper.rb
+++ app/helpers/pages_helper.rb
@@ -40,17 +40,6 @@ include PlayersHelper
     end
   end
 
-  def checkActiveCharacterTab(type, tabName)
-    requestedTab = if params[:tab].nil?
-                     'skills'
-                   else
-                     params[:tab]
-                   end
-    return 'active' if (tabName == requestedTab) && (type == 'link')
-
-    'show active' if (tabName == requestedTab) && (type == 'content')
-  end
-
   def checkActiveCampTab(type, tabName)
     requestedTab = if params[:tab].nil?
                      'general'
