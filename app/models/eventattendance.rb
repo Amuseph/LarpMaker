30ba02a15f4b5eb800b3e4fb720ea4cc4a7bc028
Amuseph <andrew.warzocha@gmail.com>
2023-05-02 09:27:17 -0400

    Go Big XP

--- app/models/eventattendance.rb
+++ app/models/eventattendance.rb
@@ -12,11 +12,25 @@ class Eventattendance < ApplicationRecord
   validates :event, uniqueness: { scope: :user }
 
   after_update :check_registration
+  after_update :check_noshow
 
   def check_registration
     if saved_change_to_registrationtype? && registrationtype != 'Player'
       self.character_id = nil
       save!
+    end
+  end
+
+  def check_noshow
+    if saved_change_to_noshow? && registrationtype != 'Player' && noshow
+      @explog = Explog.where('acquiredate BETWEEN ? AND ?', event.startdate.beginning_of_day, event.startdate.end_of_day).find_by(
+        name: 'Event', user_id: user_id
+      )
+      @explog&.destroy
+    end
+
+    if saved_change_to_noshow? && registrationtype == 'Player' && noshow
+      ApplicationController.helpers.add_feedback_exp(event, self)
     end
   end
 
