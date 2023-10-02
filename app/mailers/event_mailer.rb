10b58d444bb5bda15cad26a8ac59667990ec79e6
Amuseph <andrew.warzocha@gmail.com>
2023-10-02 18:08:06 -0400

    Bug Squash

--- app/mailers/event_mailer.rb
+++ app/mailers/event_mailer.rb
@@ -10,7 +10,7 @@ class EventMailer < ApplicationMailer
   def send_event_cast_feedback
     @feedback = params[:eventcastfeedback]
     #Andrew Warzocha | Event: 2021-08-21
-    @subject = @feedback.user.firstname + ' | (' + @feedback.event.startdate.strftime("%m/%d/%Y")
-    mail(from: 'no-reply@mythlarp.com', to: 'andrewwarzocha+w334z7hq4zywmk5hpbav@boards.trello.com', subject: @subject)
+    subject = @feedback.user.firstname + ' | (' + @feedback.event.startdate.strftime("%m/%d/%Y")
+    mail(from: 'no-reply@mythlarp.com', to: 'andrewwarzocha+w334z7hq4zywmk5hpbav@boards.trello.com', subject: subject)
   end
 end
